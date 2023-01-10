a7f67ab2487bc4d30af770c1f2840d55f1bd10ac
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-01-11 00:33:26 +0900

    [update] change icon

--- manifest.ts
+++ manifest.ts
@@ -4,7 +4,7 @@ import NotifyWorkflow from "./workflows/emoji_monitor.ts";
 export default Manifest({
   name: "slack-emoji-monitor",
   description: "slack emoji notify",
-  icon: "assets/default_new_app_icon.png",
+  icon: "assets/seikou_banzai_man.png",
   workflows: [NotifyWorkflow],
   outgoingDomains: [],
   botScopes: ["chat:write", "chat:write.public", "emoji:read"],
